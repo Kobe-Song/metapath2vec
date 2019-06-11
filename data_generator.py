4914f467edc60c3c51e40c9feeb444954168c6a3
Songkaiwen <495607958@qq.com>
2019-06-11 14:55:27 +0800

    refine with multiprocessing

--- data_generator.py
+++ data_generator.py
@@ -2,14 +2,13 @@ from collections import defaultdict
 import numpy as np
 import pickle
 from utils import AliasTable
+import multiprocessing as mp
+import argparse
 
 class DataGenerator:
-    def __init__(self):
+    def __init__(self, num_walks, walk_length):
         # user - account
-        # self.user2id = {}
-        # self.id2user = {}
-        # self.account2id = {}
-        # self.id2account = {}
+
         self.user_account = defaultdict(list)
         self.account_user = defaultdict(list)
         self.u2a_weight = defaultdict(list)
@@ -21,6 +20,9 @@ class DataGenerator:
         self.id2node = {}
         self.nid2type = {}
         self.type2nidlist = defaultdict(list)
+
+        self.num_walks = num_walks
+        self.walk_length = walk_length
 
     def load_data(self, data_file):
         '''
@@ -34,12 +36,7 @@ class DataGenerator:
                 self.account_user[lines[1]].append(lines[0])
                 self.u2a_weight[lines[0]].append(int(lines[2]))
                 self.a2u_weight[lines[1]].append(int(lines[2]))
-            # for uid, user in enumerate(self.user_account.keys()):
-            #     self.user2id[user] = uid
-            #     self.id2user[uid] = user
-            # for aid, account in enumerate(self.account_user.keys()):
-            #     self.account2id[account] = aid
-            #     self.id2account[aid] = account
+
         nid = 0
         for user in self.user_account.keys():
             self.node2id[user] = nid
@@ -67,24 +64,43 @@ class DataGenerator:
 
         return
     
-    def generate_uau(self, out_file, num_walks, walk_length):
+    def generate_uau_process(self, out_file):
         '''
         generate mata path by user-account-user
         '''
+        meta_path_seq = []
+        pool = mp.Pool()
+        meta_path_seq.extend(pool.map(self.generate_uau, (user for user in self.user_account)))
+
+        metapath_str = [''.join([n + '\t' for n in s]) + '\n' for u_seq in meta_path_seq for s in u_seq]
+        
         with open(out_file, 'w') as f:
-            for user in self.user_account:
-                for n in range(num_walks):
-                    cur_user = user
-                    f.write(cur_user)
-                    for w_len in range(walk_length):
-                        index_a = self.user_alias[cur_user].sampling()
-                        cur_a = self.user_account[cur_user][index_a]
-                        index_u = self.account_alias[cur_a].sampling()
-                        cur_user = self.account_user[cur_a][index_u]
-                        f.write('\t' + cur_a + '\t' + cur_user)
-                    f.write('\n')
+            f.writelines(metapath_str)
 
         return
+
+    
+    def generate_uau(self, user):
+        '''
+        generate mata path by user-account-user for a user
+        '''
+        user_seq = []
+        # user_seq.append(user)
+        for n in range(self.num_walks):
+            seq = []
+            seq.append(user)
+            cur_user = user
+            for w_len in range(self.walk_length):
+                index_a = self.user_alias[cur_user].sampling()
+                cur_a = self.user_account[cur_user][index_a]
+                seq.append(cur_a)
+                index_u = self.account_alias[cur_a].sampling()
+                cur_user = self.account_user[cur_a][index_u]
+                seq.append(cur_user)
+            
+            user_seq.append(seq)
+        
+        return user_seq
 
     def save_data(self):
         '''
@@ -101,18 +117,23 @@ class DataGenerator:
 
         return
 
+def parse_args():
+	#Parses the arguments.
+    parser = argparse.ArgumentParser(description="metapath sequence generator")
+    parser.add_argument('--num_walks',type=int,default=5, help='Number of walk for each node')
+    parser.add_argument('--walk_length',type=int,default=5, help='length of sequence for each walk')
+    parser.add_argument('--data_file',type=str,default='test.txt', help='input graph file')
+    parser.add_argument('--out_file',type=str,default='metapath.txt', help='output metapath sequence file')
 
+    
+    return parser.parse_args()
 
-def main():
-    data_file = 'test.txt'
-    out_file = 'metapath.txt'
-    num_walks = 2
-    walk_length = 2
-
-    generator = DataGenerator()
-    generator.load_data(data_file)
-    generator.generate_uau(out_file, num_walks, walk_length)
+def main(args):
+    generator = DataGenerator(args.num_walks, args.walk_length)
+    generator.load_data(args.data_file)
+    generator.generate_uau_process(args.out_file)
     generator.save_data()
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
